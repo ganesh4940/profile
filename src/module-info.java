89b52a3b8e956b9a4cc934e55a5fbffcf446592a
ganesh4940 <hajare640@gmail.com>
2023-01-08 19:05:37 +0530

    add project

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hajar
+ *
+ */
+module project1 {
+}
---
+++ src/project1/a1.java
@@ -0,0 +1,11 @@
+package project1;
+
+public class a1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("A1");
+
+	}
+
+}
